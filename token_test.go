dc1196bca508f5ad0d62744a322c70bcb862ec9a
alexeykhan <dev@alexeykhan.com>
2021-01-22 18:20:14 +0300

    Обновляет копирайт

--- token_test.go
+++ token_test.go
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2020 Alexey Khan
+// Copyright (c) 2021 Alexey Khan
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
